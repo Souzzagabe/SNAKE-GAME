b2bc9c5102d771063c3a864c2912e66ff2e21219
Gabriel <souzzagabe@hotmail.com>
2023-09-26 00:40:21 -0300

    finalizando funcionalidades

--- js/index.js
+++ js/index.js
@@ -1,23 +1,52 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const audio = new Audio('./assets/assets_audio.mp3')
+const score = document.querySelector(".score--value")
+const finalScore = document.querySelector(".final-score > span")
+const menu = document.querySelector(".menu-screen")
+const buttonPlay = document.querySelector(".btn-play")
 
 const size = 30;
 
-const snake = [
-    { x: 270, y: 240 }
+const initialPosition = { x: 270, y: 240 }
+
+let snake = [
+    initialPosition
 ]
 
+const incrementScore = () => (
+    score.innerText = +score.innerText + 10
+)
+
+const randomNumber = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+const randomPosition = (min, max) => {
+    const number = randomNumber(0, canvas.width - size)
+    return Math.round(number / 30) * 30
+}
+
+const randomColor = () => {
+    const red = randomNumber(0, 255)
+    const green = randomNumber(0, 255)
+    const blue = randomNumber(0, 255)
+
+    return `rgb(${red}, ${green}, ${blue})`
+}
+
 const food = {
-    x: 90,
-    y: 90,
-    color: "yellow"
+    x: randomPosition(),
+    y: randomPosition(),
+    color: randomColor()
 }
 
 let direction, loopId
 
 const drawFood = () => {
-    
+
     const { x, y, color } = food
+
     ctx.shadowColor = color
     ctx.shadowBlur = 6
     ctx.fillStyle = food.color
@@ -26,12 +55,12 @@ const drawFood = () => {
 }
 
 const drawSnake = () => {
-    ctx.fillStyle = "#ddd";
+    ctx.fillStyle = "#96C494";
 
     snake.forEach((position, index) => {
 
         if (index == snake.length - 1) {
-            ctx.fillStyle = "white";
+            ctx.fillStyle = "#10714A";
         }
 
         ctx.fillRect(position.x, position.y, size, size)
@@ -78,6 +107,55 @@ const drawGrid = () => {
     }
 }
 
+const checkEat = () => {
+    const head = snake[snake.length - 1]
+
+    if (head.x == food.x && head.y == food.y) {
+        incrementScore()
+        snake.push(head)
+        audio.play()
+
+        let x = randomPosition()
+        let y = randomPosition()
+
+        while (snake.find((position) => position.x == x && position.y == y)) {
+            x = randomPosition()
+            y = randomPosition()
+        }
+
+        food.x = x
+        food.y = y
+        food.color = randomColor()
+    }
+}
+
+const checkCollision = ()  => {
+    const head = snake[snake.length -1]
+    const canvasLimit = canvas.width - size
+    const neckIndex = snake.length -2
+
+    const wallCollisiion = 
+    head.x < 0 || head.x > canvasLimit || head.y < 0 || head.y > canvasLimit
+
+    selfCollision = snake.find((position, index) => {
+        return index < neckIndex && position.x == head.x && position.y == head.y
+    })
+
+    if (wallCollisiion || selfCollision) {
+        gameOver()
+    }
+}
+
+const gameOver = () => {
+    direction = undefined
+
+    menu.style.display = "flex"
+    finalScore.innerText = score.innerText
+    canvas.style.filter = "blur(2px"
+
+    
+}
+
 const gameLoop = () => {
     clearInterval(loopId)
 
@@ -86,10 +164,12 @@ const gameLoop = () => {
     drawFood()
     moveSnake()
     drawSnake()
+    checkEat()
+    checkCollision()
 
     loopId = setTimeout(() => {
         gameLoop()
-    }, 300)
+    }, 200)
 }
 
 gameLoop()
@@ -109,3 +189,13 @@ document.addEventListener("keydown", ({ key }) => {
     }
 })
 
+
+buttonPlay.addEventListener("click", () => {
+    score.innerText = "00"
+    menu.style.display = "none"
+    canvas.style.filter = "none"
+
+    snake = [
+        { x: 270, y: 240 },
+    ]
+})
